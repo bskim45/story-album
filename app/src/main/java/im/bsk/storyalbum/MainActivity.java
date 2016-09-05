9df79ae7c42e9d23cfd19f030c1300055690734b
Bumsoo Kim <bskim45@gmail.com>
2016-09-05 19:39:49 +0900

    basic main ui

--- app/src/main/java/im/bsk/storyalbum/MainActivity.java
+++ app/src/main/java/im/bsk/storyalbum/MainActivity.java
@@ -2,6 +2,8 @@ package im.bsk.storyalbum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                // TODO: add
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
