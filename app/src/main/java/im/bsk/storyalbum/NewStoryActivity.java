1ee53431f55b60116811bd7ed99c8895c762422c
Bumsoo Kim <bskim45@gmail.com>
2016-09-05 21:05:11 +0900

    basic new story ui

--- app/src/main/java/im/bsk/storyalbum/NewStoryActivity.java
+++ app/src/main/java/im/bsk/storyalbum/NewStoryActivity.java
@@ -1,33 +1,40 @@
 package im.bsk.storyalbum;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class NewStoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_story);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_story, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_add:
-                // TODO: launch camera
-                startActivity(new Intent(this, NewStoryActivity.class));
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.action_confirm:
+                // TODO: confirm
+                finish();
                 return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
